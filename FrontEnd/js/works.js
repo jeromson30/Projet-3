07544b4ae283377f8979d7c7739b1820d0a9b467
jeromson30 <jerome.corso@gmail.com>
2024-07-16 22:07:26 +0200

    style: CSS in accordance to figma model & page login

--- FrontEnd/js/works.js
+++ FrontEnd/js/works.js
@@ -1,44 +1,123 @@
-async function Filters() {
-    FiltersLabels = ["Tous", "Objets", "Appartements", "Hotels & restaurants"]
-    for (let i = 0; i < FiltersLabels.length; i++) {
-        const boutonFilter = document.createElement("button");
-        boutonFilter.innerText = FiltersLabels[i];
-        console.log(FiltersLabels[i]);
-        const contentBouton = document.getElementById("filters");
-        contentBouton.appendChild(boutonFilter);
-    }
-
-}
-
+let works = {};
 
 
 async function RecupInfoAPI() {
+    try {
     const result = await fetch(`http://localhost:5678/api/works`);
-           
+
     works = await result.json();
-    console.log("Résultats API reçu");	
-    // console.log(Results);
+    console.log("Données reçu de l\'API");	
+    console.log(works[0].category);
+    LoadingProjects("initial");
 
-        for (let i = 0; i < works.length; i++) {
-                console.log(works[i]);
-
-                const work = document.createElement("figure");
-                const workimg = document.createElement("img");
-                const workdesc = document.createElement("figcaption");
-                work.appendChild(workimg);
-                work.appendChild(workdesc);
-
-                workimg.setAttribute("src", works[i].imageUrl);
-                workimg.setAttribute("alt", works[i].title);
-                workdesc.innerText = works[i].title;
-
-                const content = document.getElementById("allworks");
-                content.appendChild(work);
-        }
-}
+    } catch(error) {
+        console.log("Erreur de connexion à l\'API : " + error);
+    }
+};
 
 RecupInfoAPI();
-Filters();
+
+
+
+async function Filters() {
+    FiltersLabels = works.category.name
+        for (let i = 0; i < FiltersLabels.length; i++) {
+            const boutonFilter = document.createElement("button");
+            boutonFilter.innerText = FiltersLabels[i];
+            console.log(FiltersLabels[i]);
+            const contentBouton = document.getElementById("filters");
+            contentBouton.appendChild(boutonFilter);
+        }
+
+};
+
+function LoadingProjects(Projects){
+    if(Projects === "initial"){
+        for (let i = 0; i < works.length; i++) {
+            //console.log(works[i]);
+
+            const workf = document.createElement("figure");
+            const workimg = document.createElement("img");
+            const workdesc = document.createElement("figcaption");
+            workf.appendChild(workimg);
+            workf.appendChild(workdesc);
+
+            workimg.setAttribute("src", works[i].imageUrl);
+            workimg.setAttribute("alt", works[i].title);
+            workdesc.innerText = works[i].title;
+
+            const content = document.getElementById("allworks");
+            content.appendChild(workf);
+        }
+    } else {
+        console.log("Nouvelle données : " + Projects);
+        const content = document.getElementById("allworks");
+        while (content.firstChild) {
+            content.firstChild.remove()
+        }
+
+        for (let i = 0; i < Projects.length; i++) {
+            //console.log(works[i]);
+
+            const workf = document.createElement("figure");
+            const workimg = document.createElement("img");
+            const workdesc = document.createElement("figcaption");
+            workf.appendChild(workimg);
+            workf.appendChild(workdesc);
+
+            workimg.setAttribute("src", Projects[i].imageUrl);
+            workimg.setAttribute("alt", Projects[i].title);
+            workdesc.innerText = Projects[i].title;
+
+            const content = document.getElementById("allworks");
+            content.appendChild(workf);
+        }
+    }
+}
+
+const boutonShowAll = document.querySelector("#Filter_ShowAll");
+boutonShowAll.addEventListener("click", function () {
+    //const AllWorks = Array.from(works);
+    const AllWorks = works.filter(function(work) {
+        return work.category.id > 0;
+    });
+    console.log(AllWorks);
+    LoadingProjects(AllWorks);
+});
+
+const boutonShowObjects = document.querySelector("#Filter_ShowObjects");
+boutonShowObjects.addEventListener("click", function () {
+    //const AllWorks = Array.from(works);
+    const AllObjects = works.filter(function(work) {
+        return work.category.id === 1;
+    });
+    console.log(AllObjects);
+    LoadingProjects(AllObjects);
+});
+
+const boutonShowApparts = document.querySelector("#Filter_ShowApparts");
+boutonShowApparts.addEventListener("click", function () {
+    //const AllWorks = Array.from(works);
+const AllApparts = works.filter(function(work) {
+        return work.category.id === 2;
+    });
+    console.log(AllApparts);
+    LoadingProjects(AllApparts);
+});
+
+const boutonShowHotels = document.querySelector("#Filter_ShowHotel");
+boutonShowHotels.addEventListener("click", function () {
+    //const AllWorks = Array.from(works);
+const AllHotels = works.filter(function(work) {
+        return work.category.id === 3;
+    });
+    console.log(AllHotels);
+    LoadingProjects(AllHotels);
+});
+
+
+
+//Filters();
 
 // function genererProjets(works){
 // 	for (let i = 0; i < works.length; i++) {
