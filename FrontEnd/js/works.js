e23a15df447686a62214a866b9eb8104fd9eb26c
jeromson30 <jerome.corso@gmail.com>
2024-07-17 21:35:14 +0200

    feat: Add automaticaly filters buttons by categories in API Works

--- FrontEnd/js/works.js
+++ FrontEnd/js/works.js
@@ -3,125 +3,84 @@ let works = {};
 
 async function RecupInfoAPI() {
     try {
-    const result = await fetch(`http://localhost:5678/api/works`);
+        const result = await fetch(`http://localhost:5678/api/works`);
 
-    works = await result.json();
-    console.log("Données reçu de l\'API");	
-    console.log(works[0].category);
-    LoadingProjects("initial");
-
+        works = await result.json();
+        console.log("Données reçu de l\'API");	
+        Filters();
+        LoadingProjects(0);
     } catch(error) {
-        console.log("Erreur de connexion à l\'API : " + error);
-    }
+        console.error("Oups, il y a une erreur : " + error);
+    };
 };
 
-RecupInfoAPI();
 
-
-
-async function Filters() {
-    FiltersLabels = works.category.name
-        for (let i = 0; i < FiltersLabels.length; i++) {
-            const boutonFilter = document.createElement("button");
-            boutonFilter.innerText = FiltersLabels[i];
-            console.log(FiltersLabels[i]);
-            const contentBouton = document.getElementById("filters");
-            contentBouton.appendChild(boutonFilter);
-        }
-
-};
 
 function LoadingProjects(Projects){
-    if(Projects === "initial"){
-        for (let i = 0; i < works.length; i++) {
-            //console.log(works[i]);
+    //console.log(works);
 
-            const workf = document.createElement("figure");
-            const workimg = document.createElement("img");
-            const workdesc = document.createElement("figcaption");
-            workf.appendChild(workimg);
-            workf.appendChild(workdesc);
+    let Fworks = works.filter(function(item){
+        if(Projects == 0){
+            return item.categoryId > Projects; 
+        } else {
+            return item.categoryId == Projects;
+        };
+    });
 
-            workimg.setAttribute("src", works[i].imageUrl);
-            workimg.setAttribute("alt", works[i].title);
-            workdesc.innerText = works[i].title;
-
-            const content = document.getElementById("allworks");
-            content.appendChild(workf);
-        }
-    } else {
-        console.log("Nouvelle données : " + Projects);
-        const content = document.getElementById("allworks");
-        while (content.firstChild) {
+    const content = document.getElementById("allworks");
+    while (content.firstChild) {
             content.firstChild.remove()
-        }
+    };
 
-        for (let i = 0; i < Projects.length; i++) {
-            //console.log(works[i]);
+    for (let i = 0; i < Fworks.length; i++) {            
+        const workf = document.createElement("figure");
+        const workimg = document.createElement("img");
+        const workdesc = document.createElement("figcaption");
+        workf.appendChild(workimg);
+        workf.appendChild(workdesc);
 
-            const workf = document.createElement("figure");
-            const workimg = document.createElement("img");
-            const workdesc = document.createElement("figcaption");
-            workf.appendChild(workimg);
-            workf.appendChild(workdesc);
+        workimg.setAttribute("src", Fworks[i].imageUrl);
+        workimg.setAttribute("alt", Fworks[i].title);
+        workdesc.innerText = Fworks[i].title;
 
-            workimg.setAttribute("src", Projects[i].imageUrl);
-            workimg.setAttribute("alt", Projects[i].title);
-            workdesc.innerText = Projects[i].title;
+        const content = document.getElementById("allworks");
+        content.appendChild(workf);
+    };
+};
 
-            const content = document.getElementById("allworks");
-            content.appendChild(workf);
-        }
+function Filters() {
+    const contentBouton = document.getElementById("filters");
+    const FiltersLabels = works;
+    const LabelCategorie = [];
+    //LabelCategorie.add(0);
+    LabelCategorie.push({id: 0, name: 'Tous'});
+
+    for (let i = 0; i < FiltersLabels.length; i++) {
+        if (!(FiltersLabels[i]['category']['id'] in LabelCategorie)) {
+            LabelCategorie.push({id: FiltersLabels[i]['category']['id'], name: FiltersLabels[i]['category']['name']});
+        };
+    };
+
+    for (let i = 0; i < LabelCategorie.length; i++) {
+        boutonFilter = document.createElement("button");
+        contentBouton.appendChild(boutonFilter);
+        boutonFilter.setAttribute("id", "FiltreCat");
+        boutonFilter.setAttribute("data-id", LabelCategorie[i].id);
+        boutonFilter.innerText = LabelCategorie[i].name;
+    };
+
+
+    const bouton = document.querySelectorAll("#FiltreCat");
+
+    for (let i = 0; i < bouton.length; i++) {
+        bouton[i].addEventListener("click", function() {
+            LoadingProjects(bouton[i].dataset.id);
+            //console.log("click sur : " + bouton[i].dataset.id);
+        });
     }
-}
+    
 
-const boutonShowAll = document.querySelector("#Filter_ShowAll");
-boutonShowAll.addEventListener("click", function () {
-    //const AllWorks = Array.from(works);
-    const AllWorks = works.filter(function(work) {
-        return work.category.id > 0;
-    });
-    console.log(AllWorks);
-    LoadingProjects(AllWorks);
-});
-
-const boutonShowObjects = document.querySelector("#Filter_ShowObjects");
-boutonShowObjects.addEventListener("click", function () {
-    //const AllWorks = Array.from(works);
-    const AllObjects = works.filter(function(work) {
-        return work.category.id === 1;
-    });
-    console.log(AllObjects);
-    LoadingProjects(AllObjects);
-});
-
-const boutonShowApparts = document.querySelector("#Filter_ShowApparts");
-boutonShowApparts.addEventListener("click", function () {
-    //const AllWorks = Array.from(works);
-const AllApparts = works.filter(function(work) {
-        return work.category.id === 2;
-    });
-    console.log(AllApparts);
-    LoadingProjects(AllApparts);
-});
-
-const boutonShowHotels = document.querySelector("#Filter_ShowHotel");
-boutonShowHotels.addEventListener("click", function () {
-    //const AllWorks = Array.from(works);
-const AllHotels = works.filter(function(work) {
-        return work.category.id === 3;
-    });
-    console.log(AllHotels);
-    LoadingProjects(AllHotels);
-});
+};
 
 
-
-//Filters();
-
-// function genererProjets(works){
-// 	for (let i = 0; i < works.length; i++) {
-// 		return works(i);
-// 	}
-// }
-// genererProjets(works);
+RecupInfoAPI();
